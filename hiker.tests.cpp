7903458ef7026ea21e9f2a82e0130ce231f562c7
Ben Wu <wubin28@gmail.com>
2014-10-28 03:44:48 +0000

    Created a new exercise of Game of Life.

---
+++ hiker.tests.cpp
@@ -0,0 +1,10 @@
+#include "hiker.hpp"
+#include <gtest/gtest.h>
+
+using namespace ::testing;
+
+TEST(Hiker, Life_the_universe_and_everything)
+{
+    // a simple example to start you off
+    ASSERT_EQ(42, answer());
+}
---
+++ myrsync.sh
@@ -0,0 +1,2 @@
+rsync -r /var/www/cyberdojo/katas/DA/C955933D/raccoon/sandbox/ ./
+
