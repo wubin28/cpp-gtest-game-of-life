a35b2182f91d587890d1ee1e94334ae90fb8a52f
Ben Wu <wubin28@gmail.com>
2014-10-28 05:14:02 +0000

    Wrote intention Assert of test Any_live_cell_with_fewer_than_two_live_neighbours_dies.

--- Cell.tests.cpp
+++ Cell.tests.cpp
@@ -3,13 +3,12 @@
 
 using namespace ::testing;
 
-TEST(Cell, Life_the_universe_and_everything)
+// TODO-intention-working-on: Any live cell with fewer than two live neighbours dies as if caused by under_population
+TEST(Cell, Any_live_cell_with_fewer_than_two_live_neighbours_dies_as_if_caused_by_under_population)
 {
-    // a simple example to start you off
-    ASSERT_EQ(42, answer());
+    ASSERT_EQ(typeid(DeadCell), typeid(*transitionedCell));
 }
 
-// TODO-intention: Any live cell with fewer than two live neighbours dies as if caused by under-population
 // TODO-intention: Any live cell with two or three live neighbours lives on to the next generation
 // TODO-intention: Any live cell with more than three live neighbours dies as if by overcrowding
 // TODO-intention: Any dead cell with exactly three live neighbours becomes a live cell as if by reproduction
