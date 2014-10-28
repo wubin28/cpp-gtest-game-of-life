41a400892d182a46ad2e0bce8ddc3e0e26795dfc
Ben Wu <wubin28@gmail.com>
2014-10-28 09:05:58 +0000

    Fixed Error: Implemented method CellTransitioner::transition().

--- CellTransitioner.cpp
+++ CellTransitioner.cpp
@@ -1,5 +1,12 @@
 #include "CellTransitioner.h"
+#include "Cell.h"
+#include <typeinfo>
 
 Cell* CellTransitioner::transition(Cell *cell, int numOfLiveNeighbours) {
+  if (typeid(*cell) == typeid(LiveCell)) {
+    if (numOfLiveNeighbours < 2) {
+      return new DeadCell;
+    }
+  }
   return nullptr;
 }
