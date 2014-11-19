93f5c520b2b4a3730fd439f6c0ae8b579f05fe4d
Ben Wu <wubin28@gmail.com>
2014-11-19 17:55:33 +0000

    Wrote intention code for the test 'Any_live_cell_with_fewer_than_two_live_neighbours_dies_as_if_caused_by_underpopulation'.

--- CellTest.cpp
+++ CellTest.cpp
@@ -3,12 +3,15 @@
 
 using namespace ::testing;
 
-TEST(Cell, Conways_game_of_life)
+// TODO-working-on: Any live cell with fewer than two live neighbours dies as if caused by underpopulation
+TEST(Cell, Any_live_cell_with_fewer_than_two_live_neighbours_dies_as_if_caused_by_underpopulation)
 {
-    ASSERT_EQ(42, answer());
+  LiveCell *liveCell = new LiveCell();
+
+  ASSERT_EQ(typeid(DeadCell), typeid(*liveCell->breedWithLiveNeighbours(0)));
+  ASSERT_EQ(typeid(DeadCell), typeid(*liveCell->breedWithLiveNeighbours(1)));
 }
 
-// TODO: Any live cell with fewer than two live neighbours dies as if caused by underpopulation
 // TODO: Any live cell with more than three live neighbours dies as if by overcrowding
 // TODO: Any live cell with two or three live neighbours lives on to the next generation
 // TODO: Any dead cell with exactly three live neighbours becomes a live cell
