830dbcf0ace1651e05ebe9fe4a3cf64a7148b24f
Ben Wu <wubin28@gmail.com>
2014-11-21 01:28:06 +0000

    Fixed Error: Added breeding logic for 2 or 3 live neighbours in method LiveCell::breedWithLiveNeighbours(int).

--- Cell.cpp
+++ Cell.cpp
@@ -3,6 +3,7 @@
 Cell* LiveCell::breedWithLiveNeighbours(int numberOfLiveNeighbours) {
   if (numberOfLiveNeighbours < 0 || numberOfLiveNeighbours > 8) return new ZombieCell();
   if (numberOfLiveNeighbours < 2) return new DeadCell();
+  if (numberOfLiveNeighbours == 2 || numberOfLiveNeighbours == 3) return new LiveCell();
   return new DeadCell();
 }
 
