11f6884ee332d66c199d783f2913a1a22bae681c
Ben Wu <wubin28@gmail.com>
2014-11-21 01:24:35 +0000

    Wrote user intention code for the test Any_live_cell_with_two_or_three_live_neighbours_lives_on_to_the_next_generation.

--- CellTest.cpp
+++ CellTest.cpp
@@ -3,7 +3,6 @@
 
 using namespace ::testing;
 
-// TODO-working-on: Any live cell with fewer than two live neighbours dies as if caused by underpopulation
 TEST(Cell, Any_live_cell_with_fewer_than_two_live_neighbours_dies_as_if_caused_by_underpopulation)
 {
   LiveCell *liveCell = new LiveCell();
@@ -12,6 +11,14 @@ TEST(Cell, Any_live_cell_with_fewer_than_two_live_neighbours_dies_as_if_caused_b
   ASSERT_EQ(typeid(DeadCell), typeid(*liveCell->breedWithLiveNeighbours(1)));
 }
 
-// TODO: Any live cell with more than three live neighbours dies as if by overcrowding
 // TODO: Any live cell with two or three live neighbours lives on to the next generation
+TEST(Cell, Any_live_cell_with_two_or_three_live_neighbours_lives_on_to_the_next_generation)
+{
+  LiveCell *liveCell = new LiveCell();
+
+  ASSERT_EQ(typeid(LiveCell), typeid(*liveCell->breedWithLiveNeighbours(2)));
+  ASSERT_EQ(typeid(LiveCell), typeid(*liveCell->breedWithLiveNeighbours(3)));
+}
+
+// TODO: Any live cell with more than three live neighbours dies as if by overcrowding
 // TODO: Any dead cell with exactly three live neighbours becomes a live cell
