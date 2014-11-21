decf7ff70737d103a58696640628318a6b734f55
Ben Wu <wubin28@gmail.com>
2014-11-21 00:35:28 +0000

    Wrote intention code to define method LiveCell::breedWithLiveNeighbours(int).

--- Cell.cpp
+++ Cell.cpp
@@ -1,2 +1,7 @@
 #include "Cell.hpp"
 
+Cell* LiveCell::breedWithLiveNeighbours(int numberOfLiveNeighbours) {
+  if (numberOfLiveNeighbours < 0 || numberOfLiveNeighbours > 8) return new ZombieCell();
+  if (numberOfLiveNeighbours < 2) return new DeadCell();
+  return new DeadCell();
+}
