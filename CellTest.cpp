f9792a4da29ed6cbb4825485a773ddaf9131e23c
Ben Wu <wubin28@gmail.com>
2014-11-21 01:52:57 +0000

    Refactored: Extracted test Any_live_or_dead_cell_with_invalid_number_of_live_neighbours_becomes_a_zombie_cell.

--- CellTest.cpp
+++ CellTest.cpp
@@ -3,25 +3,21 @@
 
 using namespace ::testing;
 
-TEST(Cell, Any_live_cell_with_fewer_than_two_live_neighbours_dies_as_if_caused_by_underpopulation)
-{
+TEST(Cell, Any_live_cell_with_fewer_than_two_live_neighbours_dies_as_if_caused_by_underpopulation) {
   LiveCell *liveCell = new LiveCell();
 
-  ASSERT_EQ(typeid(ZombieCell), typeid(*liveCell->breedWithLiveNeighbours(-1)));
   ASSERT_EQ(typeid(DeadCell), typeid(*liveCell->breedWithLiveNeighbours(0)));
   ASSERT_EQ(typeid(DeadCell), typeid(*liveCell->breedWithLiveNeighbours(1)));
 }
 
-TEST(Cell, Any_live_cell_with_two_or_three_live_neighbours_lives_on_to_the_next_generation)
-{
+TEST(Cell, Any_live_cell_with_two_or_three_live_neighbours_lives_on_to_the_next_generation) {
   LiveCell *liveCell = new LiveCell();
 
   ASSERT_EQ(typeid(LiveCell), typeid(*liveCell->breedWithLiveNeighbours(2)));
   ASSERT_EQ(typeid(LiveCell), typeid(*liveCell->breedWithLiveNeighbours(3)));
 }
 
-TEST(Cell, Any_live_cell_with_more_than_three_live_neighbours_dies_as_if_by_overcrowding)
-{
+TEST(Cell, Any_live_cell_with_more_than_three_live_neighbours_dies_as_if_by_overcrowding) {
   LiveCell *liveCell = new LiveCell();
 
   ASSERT_EQ(typeid(DeadCell), typeid(*liveCell->breedWithLiveNeighbours(4)));
@@ -29,15 +25,11 @@ TEST(Cell, Any_live_cell_with_more_than_three_live_neighbours_dies_as_if_by_over
   ASSERT_EQ(typeid(DeadCell), typeid(*liveCell->breedWithLiveNeighbours(6)));
   ASSERT_EQ(typeid(DeadCell), typeid(*liveCell->breedWithLiveNeighbours(7)));
   ASSERT_EQ(typeid(DeadCell), typeid(*liveCell->breedWithLiveNeighbours(8)));
-  ASSERT_EQ(typeid(ZombieCell), typeid(*liveCell->breedWithLiveNeighbours(9)));
 }
 
-// TODO-working-on: Any dead cell with exactly three live neighbours becomes a live cell
-TEST(Cell, Any_dead_cell_with_exactly_three_live_neighbours_becomes_a_live_cell)
-{
+TEST(Cell, Any_dead_cell_with_exactly_three_live_neighbours_becomes_a_live_cell) {
   DeadCell *deadCell = new DeadCell();
 
-  ASSERT_EQ(typeid(ZombieCell), typeid(*deadCell->breedWithLiveNeighbours(-1)));
   ASSERT_EQ(typeid(DeadCell), typeid(*deadCell->breedWithLiveNeighbours(0)));
   ASSERT_EQ(typeid(DeadCell), typeid(*deadCell->breedWithLiveNeighbours(1)));
   ASSERT_EQ(typeid(DeadCell), typeid(*deadCell->breedWithLiveNeighbours(2)));
@@ -47,10 +39,19 @@ TEST(Cell, Any_dead_cell_with_exactly_three_live_neighbours_becomes_a_live_cell)
   ASSERT_EQ(typeid(DeadCell), typeid(*deadCell->breedWithLiveNeighbours(6)));
   ASSERT_EQ(typeid(DeadCell), typeid(*deadCell->breedWithLiveNeighbours(7)));
   ASSERT_EQ(typeid(DeadCell), typeid(*deadCell->breedWithLiveNeighbours(8)));
-  ASSERT_EQ(typeid(ZombieCell), typeid(*deadCell->breedWithLiveNeighbours(9)));
 }
 
+TEST(Cell, Any_live_or_dead_cell_with_invalid_number_of_live_neighbours_becomes_a_zombie_cell) {
+  LiveCell *liveCell = new LiveCell();
 
+  ASSERT_EQ(typeid(ZombieCell), typeid(*liveCell->breedWithLiveNeighbours(-1)));
+  ASSERT_EQ(typeid(ZombieCell), typeid(*liveCell->breedWithLiveNeighbours(9)));
+
+  DeadCell *deadCell = new DeadCell();
+
+  ASSERT_EQ(typeid(ZombieCell), typeid(*deadCell->breedWithLiveNeighbours(-1)));
+  ASSERT_EQ(typeid(ZombieCell), typeid(*deadCell->breedWithLiveNeighbours(9)));
+}
 
 
 
